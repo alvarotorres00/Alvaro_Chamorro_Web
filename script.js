f94d8ae85cbaeb94ce215a5095114c5a19a4b878
alvarotorres00 <alvarochamorro2000@gmail.com>
2024-12-13 08:55:06 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// script.js
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Evita el envío real del formulario
+    alert('¡Gracias por leer! Espero tu contacto.');
+  });
+  
